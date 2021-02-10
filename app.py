155acb65acfba3368f2bec9e65e8114e7e5dc683
Gabriel Gonzalez <gabriel_gonzalez@yahoo.com>
2021-02-09 19:39:25 -0500

    first branch

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, redirect, url_for, render_template, request
+from models import db
+from models import create_user, login_user
+
+# Flask
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '\x14B~^\x07\xe1\x197\xda\x18\xa6[[\x05\x03QVg\xce%\xb2<\x80\xa4\x00'
+app.config['DEBUG'] = True
+
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['DEBUG'] = True
+db.init_app(app)
+
+# db.create_all()
+
+@app.route("/")
+def index():
+    return render_template("base.html")
+
+@app.route("/auth/login/", methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("/auth/login.html")
+
+    user_name = request.form.get('username')
+    user_pwd = request.form.get('password')
+
+    user = login_user(user_name, user_pwd)
+    return render_template('/auth/login.html', user=user)    
+
+
+@app.route("/auth/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template("/auth/register.html")
+    user_name = request.form.get('username')
+    user_pwd = request.form.get('password')
+
+    user = create_user(user_name, user_pwd)
+    return render_template('/auth/register.html', user=user)    
+
+if __name__ == "__main__":
+    app.run(port=3000)
---
+++ models.py
@@ -0,0 +1,53 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+
+    def __init__(self, username, password ):
+        self.username = username
+        self.password = password
+
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class ToDO(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    due_date = db.Column(db.DateTime, nullable=False)
+    completed = db.Column(db.String(1), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<ToDO %r>' % self.ToDO
+
+def create_user(username, password):
+
+    currentuser = User( username, password ) 
+
+    db.session.add(currentuser)
+
+    db.session.commit()
+
+    return currentuser
+
+def login_user(user_name, pass_word):
+    user = User('','')
+    result = user.query.filter_by(username=user_name).first()
+    if result is None:
+        print('User not found')
+    else:
+        return "User FOund"
+
+if __name__ == "__main__":
+
+    # Run this file directly to create the database tables.
+    print("Creating database tables...")
+    db.create_all()
+    print("Done!")
